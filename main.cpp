7369b7f6125802619728a0ae7f9a15b974ae1b74
Default_String <tikyikhant2334@gmail.com>
2023-03-31 14:58:57 +0630

    Endianess

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+uint32_t changeEndianess(uint32_t value);
+int main() {
+    cout << "Hello, World!" << endl;
+    uint32_t littleEndian = 0x0A0B0C0D;
+    uint32_t bigEndian = changeEndianess(littleEndian);
+    printf("%X",bigEndian);
+    return 0;
+}
+
+uint32_t changeEndianess(uint32_t value){
+    uint32_t ans = 0x00000000;
+    uint32_t temp = 0x00000000;
+    temp = value & 0x000000FF;
+    temp = temp << 24;
+    ans = ans | temp;
+
+    temp = 0x00000000;
+    temp = value & 0x0000FF00;
+    temp = temp  << 8;
+    ans = ans | temp;
+
+    temp = 0x00000000;
+    temp = value & 0x00FF0000;
+    temp = temp >> 8;
+    ans = ans | temp;
+
+    temp = 0x00000000;
+    temp = value & 0xFF000000;
+    temp = temp >> 24;
+    ans = ans | temp;
+
+    return ans;
+}
